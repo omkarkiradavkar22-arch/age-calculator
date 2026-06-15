783a72684684048e8f3efb89e1dc824ee31bbe51
omkar kirdavkar <omkarkiradavkar22@gmail.com>
2026-06-15 10:53:11 +0530

    update app file

---
+++ app.js
@@ -0,0 +1,25 @@
+const btn = document.querySelector(".btn");
+        const y = document.querySelector(".year");
+        const m = document.querySelector(".month");
+        const d = document.querySelector(".days");
+        const error = document.querySelector(".error");
+        btn.addEventListener("click",()=>{
+            let today=new Date();
+            const date = document.querySelector(".date").value;
+            if(!date){
+                error.innerHTML="Please enter the date";
+            }else{
+
+            let isdate = new Date(date);
+            console.log("button was clicked");
+            console.log("todays date is ",today);
+            console.log("date is ",isdate);
+            let year = today.getFullYear() - isdate.getFullYear();
+            let month = today.getMonth() - isdate.getMonth();
+            let days = today.getDate() - isdate.getDate();
+            console.log("age: ",year,"Years",month,"month",days,"days.");
+            y.innerHTML=year;
+            m.innerHTML=month;
+            d.innerHTML=days;
+        }
+        });
